24168716cd4e227c77d1ace5096d4a3e2226b523
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-14 22:32:15 +0530

    comment out some part of the code from loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -16,27 +17,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (exception) {
-      print(exception);
-    }
+    // try {
+    //   Position position = await Geolocator.getCurrentPosition(
+    //       desiredAccuracy: LocationAccuracy.low);
+    //   print(position);
+    // } catch (exception) {
+    //   print(exception);
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // body: Center(
-        //   // ignore: deprecated_member_use
-        //   child: RaisedButton(
-        //     onPressed: () {
-        //       getLocation();
-        //     },
-        //     child: Text('Get Location'),
-        //   ),
-        // ),
-        );
+      // body: Center(
+      //   // ignore: deprecated_member_use
+      //   child: RaisedButton(
+      //     onPressed: () {
+      //       getLocation();
+      //     },
+      //     child: Text('Get Location'),
+      //   ),
+      // ),
+    );
   }
 }
