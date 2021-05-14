b67cd7c85eaa231566316f119ca486f064d73a7b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-14 22:26:05 +0530

    initialize the field longitude inside the try block

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -9,6 +9,7 @@ class Location {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.low);
       latitude = position.latitude;
+      longitude = position.longitude;
     } catch (exception) {
       print(exception);
     }
