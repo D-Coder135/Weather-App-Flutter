b9cc2c2dd113d84a1fac94aa446734d8b8b1601d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-14 19:23:08 +0530

    cut out some part of the code

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,12 +7,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
