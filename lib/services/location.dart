f7a388e329a7b278102c831ac0942038f8125ea1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-14 22:20:59 +0530

    copy and paste some part of the code from file loading_screen.dart to file location.dart

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -4,5 +4,13 @@ class Location {
   Position latitude;
   Position longitude;
 
-  void getCurrentLocation() {}
+  void getCurrentLocation() {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (exception) {
+      print(exception);
+    }
+  }
 }
