cfcf14c765a6c4599b2bf50c456311b0e16c6c69
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-15 23:49:13 +0530

    initialize a field weatherData with the method call of the method getData() along with the await keyword

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -30,6 +30,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     longitude = location.longitude;
     NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
   }
 
   void getData() async {
