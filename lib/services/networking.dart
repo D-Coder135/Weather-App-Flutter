95fb3deeed3a5f5ebc79bc80a9ad7fbf9de85c9d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-15 23:41:58 +0530

    comment out some part of the code

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -7,8 +7,8 @@ class NetworkHelper {
   NetworkHelper(this.url);
 
   void getData() async {
-    var url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    // var url = Uri.parse(
+    //     'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     http.Response response = await http.get(url);
     if (response.statusCode == 200) {
       String data = response.body;
