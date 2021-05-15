fcd0b78d40311395e7822f6db9a4da84b452f331
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-15 20:08:26 +0530

    define a field of type double name longitude

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -14,6 +14,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   // initState() method is called as soon as the application is opened. So if you want any functionality to be performed inside your app as soon as the app the opened (eg. Location Permission) then add the method call for that functionality inside the initState() method.
 
   double latitude;
+  double longitude;
 
   @override
   void initState() {
