97d99abc2316006a7e815db192e95696120a85b1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-15 23:42:44 +0530

    pass the final field url to http.get() method inside the Uri.parse() method as a argument

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -9,7 +9,7 @@ class NetworkHelper {
   void getData() async {
     // var url = Uri.parse(
     //     'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-    http.Response response = await http.get(url);
+    http.Response response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       String data = response.body;
       var decodeData = jsonDecode(data);
