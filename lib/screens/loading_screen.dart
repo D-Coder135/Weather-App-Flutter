5fef513bd1b8e3f8fefa003b2e40076ef65c457d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-15 23:49:57 +0530

    remove some part of the code and copy the required part

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -31,17 +31,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     var weatherData = await networkHelper.getData();
-  }
-
-  void getData() async {
-      double temperature = decodeData['main'][
-          'temp']; // Method to fetch the value of a particular key from the json formatted file.
-      int conditionNumber = decodeData['weather'][0]['id'];
-      String cityName = decodeData['name'];
-      print(temperature);
-      print(conditionNumber);
-      print(cityName);
-    }
+    double temperature = decodeData['main'][
+        'temp']; // Method to fetch the value of a particular key from the json formatted file.
+    int conditionNumber = decodeData['weather'][0]['id'];
+    String cityName = decodeData['name'];
+    print(temperature);
+    print(conditionNumber);
+    print(cityName);
   }
 
   @override
