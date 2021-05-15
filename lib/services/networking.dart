47cd500f8b246c0d974192ceca76b96fc152cefb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-15 23:40:02 +0530

    copy some part of the code from loading_screen.dart file and paste it inside the method getData() inside the networking.dart file

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -6,5 +6,12 @@ class NetworkHelper {
 
   NetworkHelper(this.url);
 
-  void getData() async {}
-}
+  void getData() async {
+    var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodeData = jsonDecode(data);
+    }
+  }
