90ba890a5a2fb3063d69d55446b544a5fa02bdf7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-16 23:13:03 +0530

    remove the fields latitude and longitude from the file loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -14,9 +14,6 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   // initState() method is called as soon as the application is opened. So if you want any functionality to be performed inside your app as soon as the app the opened (eg. Location Permission) then add the method call for that functionality inside the initState() method.
 
-  double latitude;
-  double longitude;
-
   @override
   void initState() {
     super.initState();
