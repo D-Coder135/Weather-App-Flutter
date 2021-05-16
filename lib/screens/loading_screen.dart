e852faf3a302316a413c5b6d8e6cf607209c9343
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-16 23:06:09 +0530

    add some content to the url passed to the NetworkHelper() as an argument

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -30,7 +30,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     longitude = location.longitude;
 
     NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
 
     var weatherData = await networkHelper.getData();
 
