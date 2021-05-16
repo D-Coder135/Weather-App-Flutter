0ff599a236cb60ffde914808c550078e6c7272fc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 00:04:03 +0530

    pass the cityName field to the Text() widget

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -93,7 +93,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  weatherMessage,
+                  "$weatherMessage in $cityName!",
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
