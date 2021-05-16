0b870edec062c1babfe9f73885b50bea02a0c00a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-16 22:41:57 +0530

    add a constructor inside which initialized the final field inside the file location_screen.dart

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -4,6 +4,8 @@ import 'package:clima/utilities/constants.dart';
 class LocationScreen extends StatefulWidget {
   final locationWeather;
 
+  LocationScreen(this.locationWeather);
+
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
