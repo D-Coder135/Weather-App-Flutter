314fe1394e02e9011490e7f8b7cc2fd12790733d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 00:02:28 +0530

    pass the field weatherMessage to the Text() widget

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -93,7 +93,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  "It's 🍦 time in San Francisco!",
+                  weatherMessage,
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
