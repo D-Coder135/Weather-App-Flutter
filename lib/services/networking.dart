ebdcdb5d872274294a696a84856a652d218ad9d2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-16 23:32:41 +0530

    remove some comments from the file networking.dart

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -7,8 +7,6 @@ class NetworkHelper {
   NetworkHelper(this.url);
 
   Future getData() async {
-    // var url = Uri.parse(
-    //     'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     http.Response response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       String data = response.body;
