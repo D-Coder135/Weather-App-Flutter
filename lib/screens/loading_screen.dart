c96b797cf41ca5e2cbb1b280e34417097351742a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 19:09:29 +0530

    initialize a new field of type var with the method call of the getLocationWeather() method using the object field weatherModel

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -19,6 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocationData() async {
     WeatherModel weatherModel = WeatherModel();
+    var weatherData = weatherModel.getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(locationWeather: weatherData);
