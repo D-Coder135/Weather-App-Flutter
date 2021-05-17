08dafdcacba5af3a8973a34b3afc4e297c771e11
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 19:14:07 +0530

    add the await keyword along with the method call of the method getLocationWeather()

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -18,7 +18,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    var weatherData = WeatherModel().getLocationWeather();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(locationWeather: weatherData);
