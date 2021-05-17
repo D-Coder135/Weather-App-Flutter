5debd579bec661132ffc3b90520f1d389d99789f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 20:29:18 +0530

    set the value of field weatherMessage equal inside the body of the if() block

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -24,6 +24,7 @@ class _LocationScreenState extends State<LocationScreen> {
       if (weatherData == null) {
         temperature = 0;
         weatherIcon = 'Error';
+        weatherMessage = 'Unable to get weather data';
       }
       temperature = weatherData['main'][
           'temp']; // Method to fetch the value of a particular key from the json formatted file.
