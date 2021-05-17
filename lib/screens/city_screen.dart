3af1ef38babb5bed5a5e20f155da82c67d3878dc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 23:11:32 +0530

    Remove some part of the code that has been moved to the constants.dart file

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -38,24 +38,7 @@ class _CityScreenState extends State<CityScreen> {
                   style: TextStyle(
                     color: Colors.black,
                   ),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    icon: Icon(
-                      Icons.location_city,
-                      color: Colors.white,
-                    ),
-                    hintText: 'Enter City Name',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
-                      ),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
+                    decoration:
                 ),
               ),
               // ignore: deprecated_member_use
