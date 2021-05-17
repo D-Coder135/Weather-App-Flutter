862e78cde06006e11d1e78063adc20837fc8c5e3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 22:59:27 +0530

    add a TextStyle() widget inside the hintStyle property of the TextField() widget

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -36,13 +36,15 @@ class _CityScreenState extends State<CityScreen> {
                 padding: EdgeInsets.all(20.0),
                 child: TextField(
                   decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      icon: Icon(
-                        Icons.location_city,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Enter City Name'),
+                    filled: true,
+                    fillColor: Colors.white,
+                    icon: Icon(
+                      Icons.location_city,
+                      color: Colors.white,
+                    ),
+                    hintText: 'Enter City Name',
+                    hintStyle: TextStyle(),
+                  ),
                 ),
               ),
               // ignore: deprecated_member_use
