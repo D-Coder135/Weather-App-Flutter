ccb83ff65133c0555eb716363d96836f7e7ebfd8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 22:52:54 +0530

    set the fillColor property for the InputDecoration() widget

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -36,7 +36,8 @@ class _CityScreenState extends State<CityScreen> {
                 padding: EdgeInsets.all(20.0),
                 child: TextField(
                   decoration: InputDecoration(
-                      filled: true;
+                    filled: true,
+                    fillColor: Colors.white,
                   ),
                 ),
               ),
