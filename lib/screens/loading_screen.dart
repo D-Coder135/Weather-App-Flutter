e4062403054dc4a6f1eb7ca8f99662401f19b1c7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-17 19:07:52 +0530

    import the file weather.dart inside the file loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
